bc7e34e3bcdc8e141c668d939162a8d342f92c8c
Saxon Landers <saxon@ackwell.com.au>
2020-04-19 20:24:21 +1000

    Add TS support

--- packages/cli/.neutrinorc.js
+++ packages/cli/.neutrinorc.js
@@ -2,6 +2,17 @@ module.exports = {
 	use: [
 		require('@neutrinojs/node')({
 			hot: false,
-		})
+
+			babel: {
+				presets: [
+					'@babel/preset-typescript',
+				],
+			},
+		}),
+
+		neutrino => {
+			neutrino.config.resolve.extensions.add('.ts').add('.tsx')
+			neutrino.config.module.rule('compile').test(/\.(mjs|jsx?|tsx?)/)
+		},
 	]
 }
---
+++ packages/cli/src/index.ts
@@ -0,0 +1,4 @@
+function main(test: string) {
+	console.log(test)
+}
+main('cli')
